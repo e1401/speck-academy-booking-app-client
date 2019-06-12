2c6d3b5083caaad57203a4bf485bd22f74f4e664
nikola770 <nikola.trivnaovic@gmail.com>
2019-06-12 16:38:51 +0200

    Create button U REDU and style for button

--- src/UI/citizens/components/submit-request/Successfull.js
+++ src/UI/citizens/components/submit-request/Successfull.js
@@ -1,7 +1,23 @@
 import React from 'react';
 
-import { Container } from './SuccessUnsuccessStyle';
+import {
+  Container,
+  Button,
+  ButtonText,
+  StyledLink
+} from './SuccessUnsuccessStyle';
 
-const Successfull = () => <Container />;
+const Successfull = () => (
+  <Container>
+    <Title>Potvrda</Title>
+    <Description>Vaš zahtijev za rezervaciju je poslan.</Description>
+
+    <StyledLink to="">
+      <Button>
+        <ButtonText>U REDU</ButtonText>
+      </Button>
+    </StyledLink>
+  </Container>
+);
 
 export default Successfull;
--- src/UI/citizens/components/submit-request/SuccsessUnsuccessStyle.js
+++ src/UI/citizens/components/submit-request/SuccsessUnsuccessStyle.js
@@ -9,3 +9,43 @@ export const Container = styled.div`
   border: solid 1px #d9d9d9;
   background-color: #ffffff;
 `;
+
+export const StyledLink = styled(Link)`
+  text-decoration: none;
+  font-family: Poppins;
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 1.14;
+  color: #ffffff;
+  text-align: center;
+  &:focus,
+  &:hover,
+  &:visited,
+  &:link,
+  &:active {
+    text-decoration: none;
+  }
+`;
+
+export const Button = styled.div`
+  width: 374px;
+  height: 40px;
+  border-radius: 4px;
+  background-color: #0f4850;
+  color: #ffffff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 32px;
+  &:hover {
+    background-color: #229c7f;
+  }
+`;
+
+export const ButtonText = styled.p`
+  color: #fff;
+  text-transform: uppercase;
+  font-family: Poppins;
+  font-size: 14px;
+  text-decoration: none;
+`;
